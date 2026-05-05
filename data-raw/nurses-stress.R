aba7076fd2891de430a5bd4b585a83dc1d8d31dc
Astrid Wiggers <wiggers.astrid@gmail.com>
2026-05-05 12:02:39 +0200

    imported data and slightly preprocessed it

--- data-raw/nurses-stress.R
+++ data-raw/nurses-stress.R
@@ -1,3 +1,11 @@
-## code to prepare `nurses-stress` dataset goes here
+library(here)
 
-usethis::use_data(nurses-stress, overwrite = TRUE)
+# The dataset can be downloaded manually from
+# <https://drive.proton.me/urls/ZHZ6TCAN2W#e5D5U4IajdYp>.
+# Make sure to save this file to the `data-raw/` folder with the name
+# `nurses-stress.tar`.
+
+untar(
+  here("data-raw/nurses-stress.tar"),
+  exdir = here("data-raw/nurses-stress/")
+)
